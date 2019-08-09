b5cad43805acd171d863fc8777572dc7e08ff242
André Pires <38431022+piresandre@users.noreply.github.com>
2019-08-09 16:58:36 -0300

    Teste web api

---
+++ EstudoApi/Controllers/ClientesController.cs
@@ -0,0 +1,35 @@
+﻿using EstudoApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EstudoApi.Controllers
+{
+    public class ClientesController : ApiController
+    {
+        private static List<Clientes> clientes = new List<Clientes>();
+        public List<Clientes> Get()
+        {
+            return clientes;
+        }
+        [HttpPost]
+        public void Post(string nome)
+        {
+            if (!string.IsNullOrEmpty(nome))
+            {
+                clientes.Add(new Clientes(nome));
+            }
+        }
+        [HttpDelete]
+        public void Delete(string nome)
+        {
+            if (!string.IsNullOrEmpty(nome))
+            {
+                clientes.RemoveAt(clientes.IndexOf(clientes.First(x=> x.Nome.Equals(nome))));
+            }
+        }
+    }
+}
---
+++ EstudoApi/Models/Clientes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EstudoApi.Models
+{
+    public class Clientes
+    {
+        public string Nome { get; set; }
+
+        public Clientes(string texto)
+        {
+            this.Nome = texto;
+        }
+    }
+}
